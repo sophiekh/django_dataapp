7ecb52aa4490a2bfe73276bc40694223fdb344ea
sophiekh <sophieee.kh@gmail.com>
2020-04-14 19:45:41 +0300

    Change settings.py

--- pythontool/settings.py
+++ pythontool/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(file)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 

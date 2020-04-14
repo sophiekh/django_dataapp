95089da4d3c3bdd0e59ca07723b6031c50a99468
sophiekh <sophieee.kh@gmail.com>
2020-04-14 20:14:16 +0300

    Change requirements

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn toolapp.wsgi --log-file 
+web: gunicorn django-dataapp.wsgi
--- pythontool/settings.py
+++ pythontool/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -159,3 +160,6 @@ os.makedirs(STATIC_ROOT, exist_ok=True)
 STATICFILES_DIRS = (
 os.path.join(BASE_DIR, 'static'),
 )
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
